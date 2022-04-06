081bc11af042c31bf492b2bd6a1d9249a12b4af0
jonahBerlyne <jonshaber@gmail.com>
2022-04-06 15:56:32 -0400

    added auth screens

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,7 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
+interface Config {
+ apiKey: string | undefined,
+ authDomain: string | undefined,
+ projectId: string | undefined,
+ storageBucket: string | undefined,
+ messagingSenderId: string | undefined,
+ appId: string | undefined
+}
+
+const firebaseConfig: Config = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
