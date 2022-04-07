f5bee041ebc2fc41cf04acf1c4ae9673c95856c0
jonahBerlyne <jonshaber@gmail.com>
2022-04-07 14:26:03 -0400

    added home screen to app nav

--- src/Navigation/appNav.tsx
+++ src/Navigation/appNav.tsx
@@ -1,9 +1,14 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../Screens/HomeScreen';
+const {Navigator, Screen} = createNativeStackNavigator();
 
 const AppNav: FC = () => {
   return (
-    <div>appNav</div>
+    <Navigator screenOptions={{headerShown: false}}>
+      <Screen name="home" component={HomeScreen} />
+    </Navigator>
   )
 }
 
-export default AppNav
+export default AppNav;
---
+++ src/Screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React, { FC, useState } from 'react';
+import { View, Text, StyleSheet, Alert, TextInput, TouchableOpacity } from 'react-native';
+
+const HomeScreen: FC = () => {
+  return (
+    <View>
+     <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
