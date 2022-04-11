b662d3afe02d5015701d1a187029099d23aa3b27
jonahBerlyne <jonshaber@gmail.com>
2022-04-11 17:56:58 -0400

    added search icon to header

--- src/Navigation/appNav.tsx
+++ src/Navigation/appNav.tsx
@@ -1,15 +1,24 @@
 import React, { ReactElement } from 'react';
+import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { EditProfileScreen, HomeScreen, ProfileScreen, SelectPostScreen, SharePostScreen } from '../Screens';
 import { Header } from '../Components';
 import { Camera } from '../Components';
+import { AntDesign } from '@expo/vector-icons'; 
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
 const AppNav = (): ReactElement => {
   return (
     <Navigator screenOptions={{headerBackground: () => <Header />}}>
-      <Screen name="home" component={HomeScreen} options={{headerTitle: "Home", headerRight: () => <Camera />}} />
+      <Screen name="home" component={HomeScreen} options={{headerTitle: "Home", headerRight: () => {
+        return (
+          <View style={{flex: 1, flexDirection: "row"}}>
+            <Camera />
+            <AntDesign name="search1" size={24} color="black" style={{marginLeft: "10px"}} />
+          </View>
+        )
+      }}} />
       <Screen name="profile" component={ProfileScreen} options={{headerTitle: "Profile"}} />
       <Screen name="editprofile" component={EditProfileScreen} options={{headerTitle: "Edit Profile"}} />
       <Screen name="selectpost" component={SelectPostScreen} options={{headerTitle: "New post"}} />
